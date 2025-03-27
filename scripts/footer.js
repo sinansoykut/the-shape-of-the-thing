0afe28af7538286c235a772635755cdab1729538
sinansoykut <sinansoykut@gmail.com>
2025-03-27 08:38:00 -0700

    footer testing

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,4 +1,4 @@
-fetch('../components/footer.html')
+fetch('/the-shape-of-the-thing/components/footer.html')
   .then(res => res.text())
   .then(data => {
     document.getElementById('footer-placeholder').innerHTML = data;
