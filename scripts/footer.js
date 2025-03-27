ea8f7181c8233aee854e3f82d791545dd3db6a36
sinansoykut <sinansoykut@gmail.com>
2025-03-27 09:22:09 -0700

    footer path change

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,4 +1,4 @@
-=fetch(`../components/footer.html`)
+=fetch(`components/footer.html`)
   .then(res => res.text())
   .then(data => {
     document.getElementById("footer-placeholder").innerHTML = data;
